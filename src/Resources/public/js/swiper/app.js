dc16bee976786d15f9d9379c0497ad25af950b23
Mohamed Thiam <medteck15@gmail.com>
2020-11-01 23:43:22 -0500

    Makes the package "JQuery free" for Glide and Swiper.

--- src/Resources/public/js/swiper/app.js
+++ src/Resources/public/js/swiper/app.js
@@ -1,20 +1,19 @@
-(function($) {
-    $(document).ready(function () {
-        var swiper = new Swiper('#banner-slider', {
-            loop: true,
-            centeredSlides: true,
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
+document.addEventListener("DOMContentLoaded", function(event) {
+    const swiper = new Swiper('#banner-slider', {
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
-})(jQuery);
+});
+
